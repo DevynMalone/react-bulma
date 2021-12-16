b8f172a62c50d2364fe010429d706c1feb8e5d51
“DevynMalone” <Dee_malone24@yahoo.com>
2021-12-16 13:50:27 -0800

    add app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Forum from './Forum'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//======Checks to see if Dashboard tag is showing=====\\
+test('renders dashboard tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Dashboard/i);
+  expect(spanElement).toBeInTheDocument();
 });
+
+test('renders Customers tag', ()=>{
+render(<Forum />);
+const spanElement = screen.getByText(/Customers/i);
+expect(spanElement).toBeInTheDocument();
+})
+
+test('renders Authentication tag', ()=>{
+  render(<Forum />);
+  const spanElement = screen.getByText(/Authentication/i);
+  expect(spanElement).toBeInTheDocument();
+})
+
+test('renders Payments tag', ()=>{
+  render(<Forum />);
+  const spanElement = screen.getByText(/Payments/i);
+  expect(spanElement).toBeInTheDocument();
+})
+
+test('renders Transfers tag', ()=>{
+  render(<Forum />);
+  const spanElement = screen.getByText(/Transfers/i);
+  expect(spanElement).toBeInTheDocument();
+})
+
+test('renders Balance tag', ()=>{
+  render(<Forum />);
+  const spanElement = screen.getByText(/Balance/i);
+  expect(spanElement).toBeInTheDocument();
+})
+
+test('renders', ()=>{
+  render(<Forum />);
+  const spanElement = screen.getByTitle(/question/i);
+  expect(spanElement).toBeInTheDocument();
+})
